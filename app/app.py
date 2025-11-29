a8905daacafe0ceb191e5f5e6b64053344a23066
CamClendenon <camilleclendenon@gmail.com>
2025-11-29 12:04:25 -0800

    Fixed issue for incorrect upoad?

--- app/app.py
+++ app/app.py
@@ -1,10 +1,9 @@
-from flask import Flask
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from the group 7! This is our Docker + Jenkins + Terraform pipeline! We are currently "
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
