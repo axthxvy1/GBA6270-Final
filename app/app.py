659f8bf6018cc97ca0bf8e2b1cde2c336f503734
axthxvy1 <axthxvy@gmail.com>
2025-11-29 11:11:04 -0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from the group 7! This is our Docker + Jenkins + Terraform pipeline!"
+    return "Hello from the group 7! This is our Docker + Jenkins + Terraform pipeline! We are currently automating!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
